e2183d06d79f2bf00aacf513a6bd70225c13e16b
hfgvv1334 <99569748+hfgvv1334@users.noreply.github.com>
2022-03-09 21:39:40 +0700

    Add files via upload

---
+++ asset/button.js
@@ -0,0 +1,7 @@
+Let toggleButton = document.queryselector('.chuthich');
+Let subt = document.queryselector('.subct')
+
+toggleButton.addEvenListener('click' , active)
+Function active {
+   subt.classlist.toggle('active')   
+}
